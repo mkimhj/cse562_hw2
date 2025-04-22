4c549fad8fcc0d800313921d833b495467e1e3b9
Maruchi Kim <maruchi.kim@gmail.com>
2025-04-21 23:29:24 -0700

    Initial Commit

---
+++ assignment2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment2
+//
+//  Created by Maruchi Kim on 4/21/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ assignment2Tests/assignment2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  assignment2Tests.swift
+//  assignment2Tests
+//
+//  Created by Maruchi Kim on 4/21/25.
+//
+
+import Testing
+@testable import assignment2
+
+struct assignment2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
